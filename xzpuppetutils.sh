6b8a2cae645f28d942ef69ceca0f700969b2d6a9
Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2021-09-08 20:03:28 +0000

    Adding RUBYLIB=

--- xzpuppetutils.sh
+++ xzpuppetutils.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GEM_HOME=lib/vendor/bundle/ruby/2.6.0:lib   lib/puppet_x/bigbigfacts/xzpuppetutils.rb  $@
+GEM_HOME=lib/vendor/bundle/ruby/2.6.0 RUBYLIB=lib/facter/util/ruby-xz-1.0.0/lib    lib/puppet_x/bigbigfacts/xzpuppetutils.rb  $@
