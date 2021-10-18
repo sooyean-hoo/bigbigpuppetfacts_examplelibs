eb773815b7b44a9c065b8da13b6bc013bb8eedf0
Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2021-10-19 00:11:32 +0800

    Syned with the ValentePuppet

--- lib/facter/util/bigbigpuppetfacts.rb
+++ lib/facter/util/bigbigpuppetfacts.rb
@@ -1,136 +1,275 @@
-#!/opt/puppetlabs/puppet/bin/ruby
-
 require 'json'
 require 'base64'
 require 'facter'
-require_relative '../../facter/util/ruby-xz-1.0.0/lib/xz'
+require 'facter/util/ruby-xz-1.0.0/lib/xz'
 
+## Module for fact compression, compatible with Facter Cache
 module Facter::Util::Bigbigpuppetfacts
+  #  def use_compressmethod(compressmethod_chosen)
+  #    @compressmethod = compressmethod_chosen
+  #  end
+  #
+  #  def setcode(string = nil, &block)
+  #    if string.nil?
+  #      super(&block)
+  #    else
+  #      super(string, &block)
+  #    end
+  #
+  #    @code_original = @code
+  #    @code = proc do
+  #      case @compressmethod
+  #      when 'bbpf_xz'
+  #        ('bbpf_xz@' + XZ.compress(JSON.generate(@code_original.call))).force_encoding('UTF-8')
+  #      when 'bbpf_xz_base64'
+  #        ('bbpf_xz_base64@' + Base64.encode64(XZ.compress(JSON.generate(@code_original.call))))
+  #      when 'xz'
+  #        (XZ.compress(JSON.generate(@code_original.call))).force_encoding('UTF-8')
+  #      when 'xz_base64'
+  #        Base64.encode64(XZ.compress(JSON.generate(@code_original.call)))
+  #      else
+  #        @code_original.call
+  #      end
+  #    end
+  #    self
+  #  end
+  class << self
     def use_compressmethod(compressmethod_chosen)
-        @compressmethod = compressmethod_chosen
+      @compressmethod = compressmethod_chosen
     end
-    def setcode(string = nil, &block)
-        
-        if string.nil? 
-            super(&block)
-        else
-            super(string, &block)
-        end
 
-        @code_original = @code
-        @code = proc do
-            case @compressmethod
-            when 'bbpf_xz'
-                ('bbpf_xz@' + XZ.compress(   JSON.generate(@code_original.call)       )).force_encoding('UTF-8')
-            when 'bbpf_xz_base64'
-                ('bbpf_xz_base64@' + Base64.encode64(XZ.compress(   JSON.generate(@code_original.call)       )))
-            when 'xz'
-                (XZ.compress(   JSON.generate(@code_original.call)       )).force_encoding('UTF-8')
-            when 'xz_base64'
-                (Base64.encode64(XZ.compress(   JSON.generate(@code_original.call)       )))
-            else
-                @code_original.call
+    def value(user_query, compressmethod_to_use = 'auto')
+      value_direct = Facter.value(user_query)
+
+      compressmethod_to_use = @compressmethod if compressmethod_to_use == 'auto'
+
+      if %r{-dump.}.match?(user_query)
+        clear_value_direct = value_direct
+        value_direct = user_query.gsub(%r{.}, '_') ### System will use the key to know the compression
+      end
+
+      case value_direct
+      when %r{^bbpf_xz@}
+        value_direct = XZ.compress(value_direct.gsub(%r{^bbpf_xz@}, ''))
+      when %r{^bbpf_xz_base64@}
+        value_direct = XZ.decompress(Base64.decode64(value_direct.gsub(%r{^bbpf_xz_base64@}, '')))
+        value_direct = JSON.parse(value_direct)
+      when %r{xz_base64}
+        value_direct = clear_value_direct
+        value_direct = XZ.decompress(Base64.decode64(value_direct))
+        value_direct = JSON.parse(value_direct)
+      else
+        case compressmethod_to_use
+        when 'xz'
+          value_direct = XZ.compress(value_direct)
+        when 'xz_base64'
+          value_direct = XZ.decompress(Base64.decode64(value_direct))
+          value_direct = JSON.parse(value_direct)
+        else
+          value_direct
+        end
+      end
+      value_direct
+    end
+
+    def compressmethods
+      {
+        'bbpf_xz' => proc { |data| 'bbpf_xz@' + XZ.compress(data) },
+       'bbpf_xz_base64' => proc { |data| 'bbpf_xz_base64@' + Base64.encode64(XZ.compress(data)) },
+
+       'xz' => proc { |data| XZ.compress(data) },
+       'xz_base64' => proc { |data| Base64.encode64(XZ.compress(data)) },
+
+        'base64' => proc { |data| Base64.encode64(data) },
+        'json' => proc { |data|
+                    begin
+                      JSON.generate(data)
+                    rescue
+                      data
+                    end
+                  },
+        'yaml' => proc { |data|
+                    begin
+                      YAML.dump(data)
+                    rescue
+                      data
+                    end
+                  },
+
+        'plain' => proc { |data| data }
+      }
+    end
+
+    def decompressmethods
+      {
+        'bbpf_xz' => proc { |data| XZ.decompress(data.gsub(%r{^bbpf_xz@}, '')) },
+       'bbpf_xz_base64' => proc { |data| XZ.decompress(Base64.decode64(data.gsub(%r{^bbpf_xz_base64@}, ''))) },
+
+       'xz' => proc { |data| XZ.decompress(data) },
+       'xz_base64' => proc { |data| XZ.decompress(Base64.decode64(data)) },
+
+       'base64' => proc { |data| Base64.decode64(data) },
+       'json' => proc { |data|
+                   begin
+                     JSON.parse(data)
+                   rescue
+                     data
+                   end
+                 },
+       'yaml' => proc { |data|
+                   begin
+                     YAML.safe_load(data)
+                   rescue
+                     data
+                   end
+                 },
+
+        'plain' => proc { |data| data }
+      }
+    end
+
+    def decompress(data, method)
+      methodprocs = method.split('_').map { |m| decompressmethods[m] }
+      pipeprocess(data, methodprocs)
+    end
+
+    def compress(data, method)
+      methodprocs = method.split('_').reverse.map { |m| compressmethods[m] }
+      pipeprocess(data, methodprocs)
+    end
+
+    def pipeprocess(data, processpipe)
+      processpipe.reduce(data) do |data__, p|
+        p.call(data__)
+      end
+    end
+
+    def compressed_factname_info(factname, _compressmethod)
+      factname_info = "#{factname}-info"
+      factname_info
+    end
+
+    def compressed_factname_dump(factname, compressmethod)
+      factname_data = "#{factname}-dump.#{compressmethod.tr('_', '.')}"
+      factname_data
+    end
+
+    def compressed_factnames(factname, compressmethod)
+      [ compressed_factname_info(factname, compressmethod), compressed_factname_dump(factname, compressmethod) ]
+    end
+    def summarise(srcdata, summariseoptions, summarisedversion = {})
+      if summariseoptions.is_a? Hash
+        summariseoptions.each do |k0, v0|
+          if  (k0.is_a? String) && (k0 =~ %r{^/.+/$}) ## If  a regex, expand into all matches
+            p = k0.gsub(%r{^[\./]}, '').gsub(%r{[/]$}, '')
+            pattern2use = Regexp.new(p)
+            keys_matches = srcdata.select { |k1, _v1| (pattern2use.match k1) || pattern2use.match(k1.to_s) }.keys
+            # summarisedversion.merge!(hh)
+            summariseoptions_inuse = {}
+            keys_matches.each { |k_match| summariseoptions_inuse[k_match] = v0 }
+          else ## If not a regex, there will only 1 match
+            summariseoptions_inuse = { k0 => v0 }
+          end
+          summariseoptions_inuse.each do |k, v|
+            if v == '*' && !srcdata[k].nil?
+              summarisedversion[k] = srcdata[k]
+            elsif v.is_a?(Hash) && !srcdata[k].nil?
+              summarisedversion[k] = {}
+              summarise(srcdata[k], v, summarisedversion[k])
+            elsif v.is_a?(Array) && !srcdata[k].nil?
+              summarisedversion[k] = []
+              summarise(srcdata[k], v, summarisedversion[k])
+            elsif srcdata.key?(k) && !srcdata[k].nil?
+              summarisedversion[k] = srcdata[k]
+            elsif srcdata.key?(k.to_sym) && !srcdata[k.to_sym].nil?
+              summarisedversion[k] = srcdata[k.to_sym]
             end
           end
-        self
+        end
+      elsif summariseoptions.is_a? Array
+        summarisedversion = [] unless summarisedversion.is_a? Array
+        srcdata.each do |x|
+          new_e = summarise(x, summariseoptions[0])
+          summarisedversion.push(new_e)
+        end
+      end
+      summarisedversion
     end
-    class << self
-        def use_compressmethod(compressmethod_chosen)
-            @compressmethod = compressmethod_chosen
-        end
-        def value(user_query)
-            
-            value_direct=Facter.value(user_query)
-
-            if user_query =~ /-dump./
-                clear_value_direct = value_direct
-                value_direct = user_query.gsub(/./,'_')    ### System will use the key to know the compression
-            end
-
-            case value_direct
-            when /^bbpf_xz@/
-                value_direct=XZ.compress(value_direct.gsub(/^bbpf_xz@/,'') )
-            when /^bbpf_xz_base64@/
-                value_direct=XZ.decompress( Base64.decode64(  value_direct.gsub(/^bbpf_xz_base64@/,'')  ))
-                value_direct=JSON.parse(value_direct)
-            when /xz_base64/
-                value_direct = clear_value_direct
-                value_direct=XZ.decompress( Base64.decode64(  value_direct ))
-                value_direct=JSON.parse(value_direct)
-            else
-                case @compressmethod
-                when 'xz'
-                    value_direct=XZ.compress(value_direct )
-                when 'xz_base64'
-                    value_direct=XZ.decompress( Base64.decode64(  value_direct))
-                    value_direct=JSON.parse(value_direct)
-                else
-                    value_direct
-                end
-            end
-            value_direct
-        end
-
-       def compressmethods
-            {
-             'bbpf_xz' => Proc.new { | data |  'bbpf_xz@' + XZ.compress( data )   } ,
-             'bbpf_xz_base64' => Proc.new { | data | 'bbpf_xz_base64@' +  Base64.encode64(XZ.compress(data ) )  } ,
-
-             'xz' => Proc.new { | data |   XZ.compress( data )   } ,
-             'xz_base64' => Proc.new { | data |  Base64.encode64(XZ.compress(data ) )  }
-
-            }
-        end
-        def decompressmethods
-            {
-             'bbpf_xz' => Proc.new { | data |  'bbpf_xz@' + XZ.decompress( data.gsub(/^bbpf_xz@/,'') )   } ,
-             'bbpf_xz_base64' => Proc.new { | data | 'bbpf_xz_base64@' +  XZ.decompress(Base64.decode64(data.gsub(/^bbpf_xz_base64@/,'') ) )  } ,
-
-             'xz' => Proc.new { | data |   XZ.compress( data )   } ,
-             'xz_base64' => Proc.new { | data |  Base64.encode64(XZ.compress(data ) )  }
-
-            }
-        end
-
-    end
+  end
 end
-Facter::Util::Resolution.prepend  Facter::Util::Bigbigpuppetfacts
+
+module Facter::Util::Bigbigpuppetfacter
+  class << self
+    def use_compressmethod(compressmethod_chosen)
+      @compressmethod = compressmethod_chosen
+    end
+
+    def use_compressmethod_fallback(compressmethod_chosen)
+      @compressmethod_fallback = compressmethod_chosen
+    end
+
+    def compress(value, method = 'auto')
+      @compressmethod_fallback = 'plain' if @compressmethod_fallback.nil?
+      [
+        Facter::Util::Bigbigpuppetfacts.compressmethods[method],
+        Facter::Util::Bigbigpuppetfacts.compressmethods[ @compressmethod_fallback ],
+      ].compact[0].call(
+        JSON.generate(value),
+      )
+    end
+
+    def decompress(_compressed_value, method = 'auto')
+      @compressmethod_fallback = 'plain' if @compressmethod_fallback.nil?
+      [
+        Facter::Util::Bigbigpuppetfacts.decompressmethods[method],
+        Facter::Util::Bigbigpuppetfacts.decompressmethods[ @compressmethod_fallback ],
+      ].compact[0].call(
+        JSON.generate(value),
+      )
+    end
+  end
+end
+Facter::Util::Resolution.include Facter::Util::Bigbigpuppetfacter
+
+# Facter::Util::Resolution.prepend Facter::Util::Bigbigpuppetfacts
 # module Facter::Util::Bigbigpuppetfacter
-#     class << self
-#         def use_compressmethod(compressmethod_chosen)
-#             @compressmethod = compressmethod_chosen
-#         end
-#         def value(user_query)
-#             value_direct=super(user_query)
-#             case value_direct
-#             when /^bbpf_xz@/
-#                 value_direct=XZ.compress(value_direct.gsub(/^bbpf_xz@/,'') )
-#             when /^bbpf_xz_base64@/
-#                 value_direct=XZ.decompress( Base64.decode64(  value_direct.gsub(/^bbpf_xz_base64@/,'')  ))
-#                 value_direct=JSON.parse(value_direct)
-#             else
-#                 case @compressmethod
-#                 when 'xz'
-#                     value_direct=XZ.compress(value_direct )
-#                 when 'xz_base64'
-#                     value_direct=XZ.decompress( Base64.decode64(  value_direct))
-#                     value_direct=JSON.parse(value_direct)
-#                 else
-#                     value_direct
-#                 end
-#             end
-#             value_direct
-#         end
-#     end
+#  class << self
+#    def use_compressmethod(compressmethod_chosen)
+#      @compressmethod = compressmethod_chosen
+#    end
+#
+#    def value(user_query)
+#      value_direct = super(user_query)
+#      case value_direct
+#      when %r{^bbpf_xz@}
+#        value_direct = XZ.compress(value_direct.gsub(%r{^bbpf_xz@}, ''))
+#      when %r{^bbpf_xz_base64@}
+#        value_direct = XZ.decompress(Base64.decode64(value_direct.gsub(%r{^bbpf_xz_base64@}, '')))
+#        value_direct = JSON.parse(value_direct)
+#      else
+#        case @compressmethod
+#        when 'xz'
+#          value_direct = XZ.compress(value_direct)
+#        when 'xz_base64'
+#          value_direct = XZ.decompress(Base64.decode64(value_direct))
+#          value_direct = JSON.parse(value_direct)
+#        else
+#          value_direct
+#        end
+#      end
+#      value_direct
+#    end
+#  end
 # end
 # Facter.prepend Facter::Util::Bigbigpuppetfacter
-module Facter
-    class << self
-        def use_compressmethod(compressmethod_chosen)
-            Facter::Util::Bigbigpuppetfacts.use_compressmethod(compressmethod_chosen)
-        end
-        def value_a(user_query)
-            Facter::Util::Bigbigpuppetfacts.value(user_query)
-        end
-    end
-end
+# module Facter
+#  class << self
+#    def use_compressmethod(compressmethod_chosen)
+#      Facter::Util::Bigbigpuppetfacts.use_compressmethod(compressmethod_chosen)
+#    end
+#
+#    def value_a(user_query)
+#      Facter::Util::Bigbigpuppetfacts.value(user_query)
+#    end
+#  end
+# end
--- lib/puppet_x/bigbigfacts/xzpuppetutils.rb
+++ lib/puppet_x/bigbigfacts/xzpuppetutils.rb
@@ -1,158 +1,152 @@
-#!/opt/puppetlabs/puppet/bin/ruby
+#!/opt/puppetlabs/puppet/bin/ruby -I ./lib
 
+require_relative '../../facter/util/bigbigpuppetfacts.rb'
+# require 'pry-byebug'
 
-
-#require 'pry-byebug'
-
-
-
-cr=File.basename $0
-cr=cr.gsub(/.rb/,".sh")
-cr="./#{cr}"
+cr = File.basename $0
+cr = cr.gsub(%r{.rb}, '.sh')
+cr = "./#{cr}"
 
 if ARGV.count == 0
-	puts <<-helphelp
-  #{ cr } =  show this help
+  puts <<-helphelp
+  #{cr} =  show this help
 
-  #{ cr } STDIN =  read input from stdin
-  #{ cr } STDOUT =  print output
-
-
-  #{ cr } IN <PATH TO THE FILE FOR INPUT> = Read a file as input
-  #{ cr } OUT <PATH TO THE FILE FOR OUTPUT> = Read a file as output
-
-  #{ cr } IN - =  read input from stdin
-  #{ cr } OUT - =  print output
+  #{cr} STDIN =  read input from stdin
+  #{cr} STDOUT =  print output
 
 
-  #{ cr } BASE64_ENC =  Convert to Base64 conversion
-  #{ cr } BASE64_DEC =  Convert from Base64 conversion
-  #{ cr } XZ_COMP =  Do XZ compression
-  #{ cr } XZ_DECOMP =  Do XZ decompression
+  #{cr} IN <PATH TO THE FILE FOR INPUT> = Read a file as input
+  #{cr} OUT <PATH TO THE FILE FOR OUTPUT> = Read a file as output
 
-  #{ cr } CAT =  Just CAT the file
+  #{cr} IN - =  read input from stdin
+  #{cr} OUT - =  print output
+
+
+  #{cr} BASE64_ENC =  Convert to Base64 conversion
+  #{cr} BASE64_DEC =  Convert from Base64 conversion
+  #{cr} XZ_COMP =  Do XZ compression
+  #{cr} XZ_DECOMP =  Do XZ decompression
+
+  #{cr} CAT =  Just CAT the file
 
   helphelp
 
-#   { 'compress' => Facter::Util::Bigbigpuppetfacts.compressmethods.keys , 
-#     'decompress' => Facter::Util::Bigbigpuppetfacts.decompressmethods.keys }.each{ | prefix, processorKeyNames |
-# 		processorKeyNames.each { | commandpostfix |
-# 			puts <<-helphelp 
-#   #{ cr } #{prefix}_#{commandpostfix} =  #{prefix} the file using #{commandpostfix} combo-algo from "Facter::Util::Bigbigpuppetfacts"
-# 			helphelp
-# 		}
-# 		puts ''
-# 	}
+  { 'compress' => Facter::Util::Bigbigpuppetfacts.compressmethods.keys,
+    'decompress' => Facter::Util::Bigbigpuppetfacts.decompressmethods.keys }.each do |prefix, processorKeyNames|
+      processorKeyNames.each do |commandpostfix|
+        puts <<-helphelp
+  #{cr} #{prefix}_#{commandpostfix} =  #{prefix} the file using #{commandpostfix} combo-algo from "Facter::Util::Bigbigpuppetfacts"
+      helphelp
+      end
+      puts ''
+    end
 
-puts <<-helphelp 
+  puts <<-helphelp
 
 
 
   In the example, all my source file is the ./Gemfile. You can also pipe string in.
   e.g.
   This is will compress the ./Gemfile, base64 encode it and print it.
-  #{ cr } IN ./Gemfile OUT -  XZ_COMP |#{ cr } IN -  OUT - BASE64_ENC
+  #{cr} IN ./Gemfile OUT -  XZ_COMP |#{cr} IN -  OUT - BASE64_ENC
 
   This is will base64 deencode the ./Gemfile  and print it.
-  #{ cr } IN ./Gemfile OUT -  BASE64_ENC
+  #{cr} IN ./Gemfile OUT -  BASE64_ENC
 
   This is will compress the ./Gemfile, base64 encode it, then base64 deencode it, decompress and print it.
-  #{ cr } IN ./Gemfile OUT -  XZ_COMP |#{ cr } IN -  OUT - BASE64_ENC | #{ cr } IN -  OUT - BASE64_DEC | #{ cr } IN -  OUT - XZ_DECOMP
+  #{cr} IN ./Gemfile OUT -  XZ_COMP |#{cr} IN -  OUT - BASE64_ENC | #{cr} IN -  OUT - BASE64_DEC | #{cr} IN -  OUT - XZ_DECOMP
 
 
 
   This is will compress the ./Gemfile save it as  ./Gemfile.xz .
-  #{ cr } IN ./Gemfile OUT ./Gemfile.xz  XZ_COMP
+  #{cr} IN ./Gemfile OUT ./Gemfile.xz  XZ_COMP
 
   Piping Stuff in Instead of using a File, and get a compress file of the output: ./ls.xz .
-  ls -l  #{ cr } IN - OUT ./ls.xz  XZ_COMP
+  ls -l  #{cr} IN - OUT ./ls.xz  XZ_COMP
 
 
   In Linux:::::
   For benchmarking, we can use the linux command: time. The BenchMark Gem is not used, as this can separate out the ruby processing from the Benchmarking's,
-  Also note that we are piping the data into a null. This will help to give us a better view of the performance, as it removes the I/O's contribution. 
+  Also note that we are piping the data into a null. This will help to give us a better view of the performance, as it removes the I/O's contribution.#{' '}
 
-  > time  #{ cr } IN ./Gemfile OUT /dev/null  XZ_COMP
+  > time  #{cr} IN ./Gemfile OUT /dev/null  XZ_COMP
 
-  > time #{ cr } IN ./Gemfile OUT -  XZ_COMP |#{ cr } IN -  OUT - BASE64_ENC | #{ cr } IN -  OUT - BASE64_DEC | #{ cr } IN -  OUT /dev/null XZ_DECOMP
+  > time #{cr} IN ./Gemfile OUT -  XZ_COMP |#{cr} IN -  OUT - BASE64_ENC | #{cr} IN -  OUT - BASE64_DEC | #{cr} IN -  OUT /dev/null XZ_DECOMP
 
-  Here is the version with the I/O 
-  > time #{ cr } IN ./Gemfile OUT -  XZ_COMP |#{ cr } IN -  OUT - BASE64_ENC | #{ cr } IN -  OUT - BASE64_DEC | #{ cr } IN -  OUT ./Gemfile.xz XZ_DECOMP
+  Here is the version with the I/O#{' '}
+  > time #{cr} IN ./Gemfile OUT -  XZ_COMP |#{cr} IN -  OUT - BASE64_ENC | #{cr} IN -  OUT - BASE64_DEC | #{cr} IN -  OUT ./Gemfile.xz XZ_DECOMP
 
 helphelp
 else
-#	binding.pry
+  #	binding.pry
 
-	if ARGV.include?('IN')
-		infname=ARGV.reduce(''){ |x_whichfollow,x|
-			x_whichfollow = x if  x_whichfollow == 'IN'
-			x_whichfollow = x if  x == 'IN'
-			x_whichfollow
-		}
-		#infname=infname[0] unless infname.nil? || infname.empty?
+  if ARGV.include?('IN')
+    infname = ARGV.reduce('') do |x_whichfollow, x|
+      x_whichfollow = x if  x_whichfollow == 'IN'
+      x_whichfollow = x if  x == 'IN'
+      x_whichfollow
+    end
+    # infname=infname[0] unless infname.nil? || infname.empty?
 
-		unless infname.nil? || infname.empty? || infname == '-'
-			file = File.open(infname)
-			data=file.read
-			file.close
-		end
-	end
-	data=STDIN.read if ARGV.include?('STDIN') || infname == '-'
+    unless infname.nil? || infname.empty? || infname == '-'
+      file = File.open(infname)
+      data = file.read
+      file.close
+    end
+  end
+  data = STDIN.read if ARGV.include?('STDIN') || infname == '-'
 
+  if ARGV.include?('OUT')
+    outfname = ARGV.reduce('') do |x_whichfollow, x|
+      x_whichfollow = x if  x_whichfollow == 'OUT'
+      x_whichfollow = x if  x == 'OUT'
+      x_whichfollow
+    end
+    # outfname=outfname[0] unless outfname.nil? || outfname.empty?
+  end
+  outoutSTDOUT = ARGV.include?('STDOUT') || outfname == '-'
 
-	if ARGV.include?('OUT')
-		outfname=ARGV.reduce(''){ |x_whichfollow,x|
-			x_whichfollow = x if  x_whichfollow == 'OUT'
-			x_whichfollow = x if  x == 'OUT'
-			x_whichfollow
-		}
-		#outfname=outfname[0] unless outfname.nil? || outfname.empty?
-	end
-	outoutSTDOUT = ARGV.include?('STDOUT') || outfname == '-'
+  # when  ARGV.include?('CAT')
+  # 	puts(data)
+  if ARGV.include?('BASE64_ENC')
+    require 'base64'
+    data = Base64.encode64(data)
+  elsif  ARGV.include?('BASE64_DEC')
+    require 'base64'
+    data = Base64.decode64(data)
 
-	case
-	# when  ARGV.include?('CAT')
-	# 	puts(data)
-	when  ARGV.include?('BASE64_ENC')
-		require 'base64'
-		data = Base64.encode64(data)
-	when  ARGV.include?('BASE64_DEC')
-		require 'base64'
-		data = Base64.decode64(data)
+  elsif  ARGV.include?('XZ_COMP')
+    require 'xz'
+    data = XZ.compress(data)
+  elsif  ARGV.include?('XZ_DECOMP')
+    require 'xz'
+    data = XZ.decompress(data)
 
-	when  ARGV.include?('XZ_COMP')
-		require 'xz'
-		data = XZ.compress(data)
-	when  ARGV.include?('XZ_DECOMP')
-		require 'xz'
-		data = XZ.decompress(data)
+  elsif  ARGV.include?('BZ_COMP')
+    require 'bzip2'
+    data = Bzip2.compress(data)
+  elsif  ARGV.include?('BZ_DECOMP')
+    require 'bzip2'
+    data = Bzip2.uncompress(data)
+  else
+    # ＃# Using Procs from Facter::Util::Bigbigpuppetfacts
+    { 'compress_' => Facter::Util::Bigbigpuppetfacts.compressmethods,
+      'decompress_' => Facter::Util::Bigbigpuppetfacts.decompressmethods }.each do |prefix, processorhash|
+      processor = processorhash.select do |pname, _p|
+        ARGV.include?(prefix + pname)
+      end
+      unless processor.nil? || processor.empty?
+        processor = processor[ processor.keys[0] ]
+        data = processor.call(data) unless processor.nil?
+      end
+    end
+  end
 
-	when  ARGV.include?('BZ_COMP')
-		require 'bzip2'
-		data = Bzip2.compress(data)
-	when  ARGV.include?('BZ_DECOMP')
-		require 'bzip2'
-		data = Bzip2.uncompress(data)
-	else
-		require_relative '../../facter/util/bigbigpuppetfacts.rb'
-		#＃# Using Procs from Facter::Util::Bigbigpuppetfacts
-		{ 'compress_' => Facter::Util::Bigbigpuppetfacts.compressmethods, 
-		  'decompress_' => Facter::Util::Bigbigpuppetfacts.decompressmethods }.each{ | prefix, processorhash |
-			processor = processorhash.select { | pname, p |
-				ARGV.include?( prefix + pname )
-			}
-			unless processor.nil? || processor.empty?
-				processor = processor[  processor.keys[0] ] 
-				data = processor.call(data) unless processor.nil?
-			end
-		}
-	end
-
-	unless outoutSTDOUT
-		file = File.open(outfname, "w")
-		file.write(data)
-		file.close
-	else
-		puts(data)
-	end
+  if outoutSTDOUT
+    puts(data)
+  else
+    file = File.open(outfname, 'w')
+    file.write(data)
+    file.close
+  end
 end
