724268713c676e9334eda2f2f436249e6248c716
Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2021-10-22 11:09:04 +0800

    Linted and with TestData in heredoc
    Signed-off-by: Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

--- lib/facter/util/bigbigpuppetfacts.rb
+++ lib/facter/util/bigbigpuppetfacts.rb
@@ -74,20 +74,21 @@ module Facter::Util::Bigbigpuppetfacts
       end
       value_direct
     end
-    def autoload_declare
-      libPath = File.join(File.dirname(__FILE__), "./rbzip2-0.3.0/lib/")
-      $LOAD_PATH << libPath unless  $LOAD_PATH.include?(libPath)
 
-      libPath = File.join(File.dirname(__FILE__), "./ruby-xz-1.0.0/lib/")
-      $LOAD_PATH << libPath unless  $LOAD_PATH.include?(libPath)
+    def autoload_declare
+      libPath = File.join(File.dirname(__FILE__), './rbzip2-0.3.0/lib/')
+      $LOAD_PATH << libPath unless $LOAD_PATH.include?(libPath)
+
+      libPath = File.join(File.dirname(__FILE__), './ruby-xz-1.0.0/lib/')
+      $LOAD_PATH << libPath unless $LOAD_PATH.include?(libPath)
 
       libPath = File.dirname(__FILE__)
-      $LOAD_PATH << libPath unless  $LOAD_PATH.include?(libPath)
+      $LOAD_PATH << libPath unless $LOAD_PATH.include?(libPath)
 
       autoload :XZ, 'xz'
       autoload :RBzip2, 'rbzip2'
-
     end
+
     # :: denote sub compression methods
     def compressmethods
       autoload_declare
@@ -98,31 +99,30 @@ module Facter::Util::Bigbigpuppetfacts
        'xz' => proc { |data| XZ.compress(data) },
        'xz_base64' => proc { |data| Base64.encode64(XZ.compress(data)) },
 
-
        'bz2::ffi' => proc { |data|
-          dfile = StringIO.new('')
-          bz2 = RBzip2::FFI::Compressor.new(dfile) # wrap the file into the compressor
-          bz2.write data # write the raw data to the compressor
-          bz2.close
-          data = dfile.string
-          data
-        },
+                       dfile = StringIO.new('')
+                       bz2 = RBzip2::FFI::Compressor.new(dfile) # wrap the file into the compressor
+                       bz2.write data # write the raw data to the compressor
+                       bz2.close
+                       data = dfile.string
+                       data
+                     },
         'bz2::java' => proc { |data|
-           dfile = StringIO.new('')
-           bz2 = RBzip2::Java::Compressor.new(dfile) # wrap the file into the compressor
-           bz2.write data # write the raw data to the compressor
-           bz2.close
-           data = dfile.string
-           data
-         },
+                         dfile = StringIO.new('')
+                         bz2 = RBzip2::Java::Compressor.new(dfile) # wrap the file into the compressor
+                         bz2.write data # write the raw data to the compressor
+                         bz2.close
+                         data = dfile.string
+                         data
+                       },
         'bz2::ruby' => proc { |data|
-           dfile = StringIO.new('')
-           bz2 = RBzip2::Ruby::Compressor.new(dfile) # wrap the file into the compressor
-           bz2.write data # write the raw data to the compressor
-           bz2.close
-           data = dfile.string
-           data
-         },
+                         dfile = StringIO.new('')
+                         bz2 = RBzip2::Ruby::Compressor.new(dfile) # wrap the file into the compressor
+                         bz2.write data # write the raw data to the compressor
+                         bz2.close
+                         data = dfile.string
+                         data
+                       },
 
         'bz2::auto' => proc { |data|
           dfile = StringIO.new('')
@@ -239,19 +239,22 @@ module Facter::Util::Bigbigpuppetfacts
     end
 
     def testdata
-      ["SOOYEANISTESTINGCOMPRESSFACTWITH THIS STRING=+_` <[{ 'aaaa':'a0/s'}]>|!/?",
-       "@The quick brown fox jumps over the lazy dog's back",
-       "@THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'S BACK 1234567890@",
+      t = <<-TTTTTDATA
+      SOOYEANISTESTINGCOMPRESSFACTWITH THIS STRING=+_` <[{ 'aaaa':'a0/s'}]>|!/?
+       @The quick brown fox jumps over the lazy dog's back
+       @THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG'S BACK 1234567890@
 
-       "@Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ",
-        "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim",
-        " ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
-        " aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
-        " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
-        " deserunt mollit anim id est laborum.@",
+       @Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+       ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+       aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+       in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+       deserunt mollit anim id est laborum.@
 
-       "@\#\$#%$@^%$#^%^$&^&%*&*(&^(&^)*&^)*&`\"\\"].join ''
+       @\#\$#%$@^%$#^%^$&^&%*&*(&^(&^)*&^)*&`\"\\"
+      TTTTTDATA
+      t
     end
 
     def checkmethod(method, fallback_method = 'plain')
