312db90757e3fcc83e2b6542e8a385350981e4f7
Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2021-10-30 17:43:09 +0000

    Test to do checking on the system for consideration of making this a helper custom fact.
    Signed-off-by: Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

--- examples/tests/testbigbigpuppetfacts.rb
+++ examples/tests/testbigbigpuppetfacts.rb
@@ -19,7 +19,7 @@ bb = BBPFTester.new
 fallback_methods= 'plain'
 method2set = 'xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
-bb.use_compressmethod_fallback 'plain'
+bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
 puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -31,7 +31,7 @@ puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 fallback_methods= 'plain'
 method2set = 'xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
-bb.use_compressmethod_fallback 'plain'
+bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
 puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -42,7 +42,7 @@ puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 fallback_methods= 'plain'
 method2set = 'bz2_base64,xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
-bb.use_compressmethod_fallback 'plain'
+bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
 puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -53,18 +53,38 @@ puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 fallback_methods= 'plain'
 method2set = 'gz_base64,bz2_base64,xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
-bb.use_compressmethod_fallback 'plain'
+bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
 puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
 
 
 # Use case 5
 fallback_methods= 'gz_base64,bz2_base64,xz_base64,plain'
 method2set = 'shits'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
-bb.use_compressmethod_fallback 'plain'
+bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
 puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+
+
+# Use case 6
+bb.use_compressmethod_fallback fallback_methods
+methods_to_check="gz_base64,bz2_base64,xz_base64,plain,7z_base74,lzma_base64"
+methods_to_check=methods_to_check.split(',')
+
+# methodshashs_to_check=methods_to_check.reduce({}){  | rethash, m|
+#   rethash[methods_to_check[0]]=methods_to_check
+#   methods_to_check=methods_to_check.rotate
+#   rethash
+# }
+methodshashs_to_check=methods_to_check.reduce({}){  | rethash, m|
+  bb.use_compressmethod(m)
+  rethash[m]=m==bb.compressmethod_used ? 'Supported' : "Not Supported"
+  rethash
+}
+
+
+puts methodshashs_to_check.to_s
