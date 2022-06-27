0cac323dc95a6f60154d55e12a0351ccd54cd920
ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2022-06-27 13:06:02 +0800

    Now switching to new naming convention at facter level
    Signed-off-by: ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

--- lib/facter/util/bigbigpuppetfacts.rb
+++ lib/facter/util/bigbigpuppetfacts.rb
@@ -660,6 +660,13 @@ module Facter::Util::Bigbigpuppetfacter
       Facter::Util::Bigbigpuppetfacts.decompressmethods['::error'].call(value)
     end
   end
+
+    alias encode compress
+    alias decode decompress
+
+    alias use_encodemethod_fallback use_compressmethod_fallback
+    alias use_encodemethod use_compressmethod
+    alias encodemethod_used compressmethod_used
 end
 Facter::Util::Resolution.include Facter::Util::Bigbigpuppetfacter
 Facter::Core::Aggregate.include Facter::Util::Bigbigpuppetfacter
