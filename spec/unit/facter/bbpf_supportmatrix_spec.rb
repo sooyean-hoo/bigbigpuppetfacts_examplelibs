713f62af7be215c833d1d59698e4f1de36ac98b1
ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2022-07-04 18:19:51 +0800

    Add in matrix tests
    Signed-off-by: ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

---
+++ spec/unit/facter/bbpf_supportmatrix_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+## Before running this test...
+## Run the following preparation code first at the module root folder
+# bbpfcodedir=$PWD/../`basename $PWD ` ; mkdir -p /tmp/media ;  cd  /tmp/media ; rsync -avvphrz $bbpfcodedir ./ ; cd `basename $bbpfcodedir ` ; git reset --hard ; git checkout 4publicversion ;
+
+lib_path = File.join(File.dirname(__FILE__), '../../../lib/')
+$LOAD_PATH << lib_path unless $LOAD_PATH.include?(lib_path)
+
+lib_path = '/tmp/media/bigbigpuppetfacts/lib/'
+$LOAD_PATH << lib_path unless $LOAD_PATH.include?(lib_path)
+
+require 'spec_helper'
+require 'facter'
+require 'facter/bbpf_supportmatrix'
+
+describe :bbpf_supportmatrix, type: :fact do
+  subject(:fact) { Facter.fact(:bbpf_supportmatrix) }
+
+  before :each do
+    # perform any action that should be run before every test
+    Facter.clear
+  end
+
+  it 'returns a value' do
+    expect(fact.value).to include("support")
+  end
+end
