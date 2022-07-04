28bd5b212e4ce9bf136f35dcc501aca811a69621
ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2022-07-04 17:53:33 +0800

    Add in proper tests with PDK validated
    Signed-off-by: ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

--- lib/tests/bbpf_addons/testbigbigpuppetfacts.rb
+++ lib/tests/bbpf_addons/testbigbigpuppetfacts.rb
@@ -4,7 +4,6 @@
 ## Run the following preparation code first at the module root folder
 # bbpfcodedir=$PWD/../`basename $PWD ` ; mkdir -p /tmp/media ;  cd  /tmp/media ; rsync -avvphrz $bbpfcodedir ./ ; cd `basename $bbpfcodedir ` ; git reset --hard ; git checkout 4publicversion ;
 
-
 lib_path = File.join(File.dirname(__FILE__), '../../lib/')
 $LOAD_PATH << lib_path unless $LOAD_PATH.include?(lib_path)
 
@@ -14,18 +13,15 @@ $LOAD_PATH << lib_path unless $LOAD_PATH.include?(lib_path)
 require 'json'
 require 'facter/util/bigbigpuppetfacts'
 
-
-
-
 ##### Test Setup
 class BBPFTester
   include Facter::Util::Bigbigpuppetfacter
 
-  def bbpf_supportmatrixtest()
+  def bbpf_supportmatrixtest
+    methods_to_check = [ 'xz', 'xz_base64' ]
 
-    methods_to_check = [ 'xz' , 'xz_base64' ]
-
-    methodshashs_to_check = methods_to_check.uniq.each_with_object({}) do |m, rethash|
+    # methodshashs_to_check =
+    methods_to_check.uniq.each_with_object({}) do |m, rethash|
       hash_key = m.match?(%r{^[\^]}) ? "plain_#{m.gsub(%r{^[\^]}, '')}" : m
 
       begin
@@ -36,94 +32,79 @@ class BBPFTester
       end
     end
   end
-
 end
 bb = BBPFTester.new
 
-
-result=bb.bbpf_supportmatrixtest
+result = bb.bbpf_supportmatrixtest
+puts JSON.pretty_generate(result)
 
 # Use case 0 Current
-fallback_methods= 'plain'
+fallback_methods = 'plain'
 method2set = 'bz2_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
 bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
-puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-
+puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 # Use case 1
-fallback_methods= 'plain'
+fallback_methods = 'plain'
 method2set = 'xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
 bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
-puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-
-
+puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 # Use case 2
-fallback_methods= 'plain'
+fallback_methods = 'plain'
 method2set = 'xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
 bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
-puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-
+puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 # Use case 3
-fallback_methods= 'plain'
+fallback_methods = 'plain'
 method2set = 'bz2_base64,xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
 bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
-puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-
+puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 # Use case 4
-fallback_methods= 'plain'
+fallback_methods = 'plain'
 method2set = 'gz_base64,bz2_base64,xz_base64'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
 bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
-puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
+puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 # Use case 5
-fallback_methods= 'gz_base64,bz2_base64,xz_base64,plain'
+fallback_methods = 'gz_base64,bz2_base64,xz_base64,plain'
 method2set = 'shits'
 puts "==fallback_methods=#{fallback_methods}=\n=method2set=#{method2set}="
 bb.use_compressmethod_fallback fallback_methods
 bb.use_compressmethod('^json_' + method2set)
 puts "==bb.compressmethod_used=#{bb.compressmethod_used}="
-puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-
+puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 # Use case 6
 bb.use_compressmethod_fallback fallback_methods
-methods_to_check="gz_base64,bz2_base64,xz_base64,plain,7z_base74,lzma_base64"
-methods_to_check=methods_to_check.split(',')
+methods_to_check = 'gz_base64,bz2_base64,xz_base64,plain,7z_base74,lzma_base64'
+methods_to_check = methods_to_check.split(',')
 
 # methodshashs_to_check=methods_to_check.reduce({}){  | rethash, m|
 #   rethash[methods_to_check[0]]=methods_to_check
 #   methods_to_check=methods_to_check.rotate
 #   rethash
 # }
-methodshashs_to_check=methods_to_check.reduce({}){  | rethash, m|
+methodshashs_to_check = methods_to_check.each_with_object({}) do |m, rethash|
   bb.use_compressmethod(m)
-  rethash[m]=m==bb.compressmethod_used ? 'Supported' : "Not Supported"
-  rethash
-}
+  rethash[m] = m == bb.compressmethod_used ? 'Supported' : 'Not Supported'
+end
 
-
-puts  JSON.pretty_generate(methodshashs_to_check)
+puts JSON.pretty_generate(methodshashs_to_check)
