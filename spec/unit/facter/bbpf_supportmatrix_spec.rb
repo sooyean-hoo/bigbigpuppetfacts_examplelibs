a4aae28f5a3984f2e53eb37696815563f38de890
ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2022-07-06 19:32:54 +0800

    Start of the new addon module... ALL CLEAN UP
    Signed-off-by: ID-19790207 Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>

--- spec/unit/facter/bbpf_supportmatrix_spec.rb
+++ spec/unit/facter/bbpf_supportmatrix_spec.rb
@@ -2,7 +2,8 @@
 
 ## Before running this test...
 ## Run the following preparation code first at the module root folder
-# bbpfcodedir=$PWD/../`basename $PWD ` ; mkdir -p /tmp/media ;  cd  /tmp/media ; rsync -avvphrz $bbpfcodedir ./ ; cd `basename $bbpfcodedir ` ; git reset --hard ; git checkout 4publicversion ;
+## OLD bbpfcodedir=$PWD/../`basename $PWD ` ; mkdir -p /tmp/media ;  cd  /tmp/media ; rsync -avvphrz $bbpfcodedir ./ ; cd `basename $bbpfcodedir ` ; git reset --hard ; git checkout 4publicversion ;
+# bbpfcodedir=$PWD/../`basename $PWD ` ; mkdir -p /tmp/media ;  cd  /tmp/media ; git clone $bbpfcodedir    ; cd `basename $bbpfcodedir ` ; git reset --hard ; git checkout 4publicversion ;
 
 lib_path = File.join(File.dirname(__FILE__), '../../../lib/')
 $LOAD_PATH << lib_path unless $LOAD_PATH.include?(lib_path)
@@ -26,9 +27,6 @@ describe :bbpf_supportmatrix, type: :fact do
   end
 
   it 'returns a value' do
-    expect(fact.value).to include({ 'xz_base64' => 'Supported' })
-    expect(fact.value).to include({ 'xz' => 'Supported' })
-    expect(fact.value).to include({ 'bz2' => 'Supported' })
     expect(fact.value).to include({ 'base64' => 'Supported' })
     expect(fact.value).to include({ 'gz' => 'Supported' })
   end
